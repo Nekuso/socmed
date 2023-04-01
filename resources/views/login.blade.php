2e3d997b3f84b67f1804682293da50ab71477a8b
Nixxo <92588172+Nekuso@users.noreply.github.com>
2023-04-01 23:53:06 +0800

    Updated

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,83 +2,57 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login</title>
-  @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    @vite('resources/css/login.css')
+
 </head>
 
 <body>
-  <section class="vh-100" style="background-color: #9A616D;">
-    <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col col-xl-10">
-          <div class="card" style="border-radius: 1rem;">
-            <div class="row g-0">
-              <div class="col-md-6 col-lg-5 d-none d-md-block">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-              </div>
-              <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                <div class="card-body p-4 p-lg-5 text-black">
-                  @if ($errors->any())
-                  <div class="alert alert-danger">
+    <div class="container">
+        <div class="left">
+            <div class="login-section">
+                @if ($errors->any())
+                <div class="alert alert-danger">
                     <ul>
-                      @foreach ($errors->all() as $error)
-                      <li>{{ $error }}</li>
-                      @endforeach
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
                     </ul>
-                  </div>
-                  @endif
-                  <form method="post" action="{{ route('signIn') }}">
+                </div>
+                @endif
+                <header>
+                    <h2 class="animation a1">Login</h2>
+                    <h4 class="animation a2">
+                        Welcome back, Please login to your account
+                    </h4>
+                </header>
+                <form method="post" action="{{ route('signIn') }}">
                     @if (Session::has('success'))
                     <div class="alert alert-success">
-                      {{ Session::get('success') }}
+                        {{ Session::get('success') }}
                     </div>
                     @endif
                     @if (Session::has('fail'))
                     <div class="alert alert-danger">
-                      {{ Session::get('fail') }}
+                        {{ Session::get('fail') }}
                     </div>
                     @endif
                     @csrf
-                    <div class="d-flex align-items-center mb-3 pb-1">
-                      <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                      <span class="h1 fw-bold mb-0">Login</span>
-                    </div>
-
-                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-
-                    <div class="form-outline mb-4">
-                      <input type="email" id="email" name="email" class="form-control form-control-lg" />
-                      <label class="form-label" for="email">Email address</label>
-                    </div>
-
-                    <div class="form-outline mb-4">
-                      <input type="password" id="password" name="password" class="form-control form-control-lg" />
-                      <label class="form-label" for="password">Password</label>
-                    </div>
-
-                    <div class="pt-1 mb-4">
-                      <button class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
-                    </div>
-
-                    <a class="small text-muted" href="#!">Forgot password?</a>
-                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account?
-                      <a href="{{ route('signup') }}" style="color: #393f81;">Register here</a>
-                    </p>
-                    <a href="#!" class="small text-muted">Terms of use.</a>
-                    <a href="#!" class="small text-muted">Privacy policy</a>
-                  </form>
-
-                </div>
-              </div>
+                    <input type="email" id="email" name="email" placeholder="Email" class="input-field animation a3" />
+                    <input type="password" id="password" name="password" placeholder="Password" class="input-field animation a4" />
+                    <p class="animation a5">Dont have an account?<a href="{{ route('signup') }}">Sign up</a></p>
+                    <button class="animation a6" type="submit">Login</button>
+                </form>
             </div>
-          </div>
         </div>
-      </div>
+        <div class="right"></div>
     </div>
-  </section>
 </body>
 
 </html>
