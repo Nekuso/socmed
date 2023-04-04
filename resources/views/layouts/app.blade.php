2d0d866d666cece8d5c8d3b72b578e9d146ee07b
Nixxo <92588172+Nekuso@users.noreply.github.com>
2023-04-04 19:35:48 +0800

    Updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/solid.css" integrity="sha384-Tv5i09RULyHKMwX0E8wJUqSOaXlyu3SQxORObAI08iUwIalMmN5L6AvlPX2LMoSE" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous" />
-    <title>@yield('title')</title>
-    @vite('resources/css/app.css')
-    <style>
+    <title>@yield('title')</title> @vite('resources/css/app.css') <style>
         /* CSS for profile */
         .profile {
             padding: 20px;
@@ -37,35 +35,23 @@
 
 <body>
     <nav class="navbar navbar-expand-lg fixed-top bg-dark navbar-dark">
-        <div class="container">
-            <a class="navbar-brand" href="{{route('home')}}">Nekuso</a>
-            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
+        <div class="container"> <a class="navbar-brand" href="{{route('home')}}">Nekuso</a> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto align-items-center">
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="{{route('home')}}" style="font-size: .7rem; display: flex; align-items: center;"><i class="fas fa-plus-circle pe-2"></i>Post</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="#!" style="font-size: .7rem; display: flex; align-items: center;"><i class="fas fa-bell pe-2"></i>Alerts</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="{{route('users_list')}}" style="font-size: .7rem; display: flex; align-items: center;"><i class="fas fa-user pe-2"></i>Users</a>
-                    </li>
-                    <li class="nav-item ms-3">
-                        <a class="btn btn-light btn-rounded" href="{{route('logout')}}" style="font-family: 'Press Start 2P', cursive; font-size: .7rem;">Sign out</a>
-                    </li>
+                    <li class="nav-item"> <a class="nav-link mx-2" href="{{route('home')}}" style="font-size: .7rem; display: flex; align-items: center;"><i class="fas fa-plus-circle pe-2"></i>Post</a> </li>
+                    <li class="nav-item"> <a class="nav-link mx-2" href="#!" style="font-size: .7rem; display: flex; align-items: center;"><i class="fas fa-bell pe-2"></i>Alerts</a> </li>
+                    <li class="nav-item"> <a class="nav-link mx-2" href="{{route('users_list')}}" style="font-size: .7rem; display: flex; align-items: center;"><i class="fas fa-user pe-2"></i>Users</a> </li>
+                    <li class="nav-item ms-3"> <a class="btn btn-light btn-rounded" href="{{route('logout')}}" style="font-family: 'Press Start 2P', cursive; font-size: .7rem;">Sign out</a> </li>
                 </ul>
             </div>
         </div>
     </nav>
-
     @yield('content')
 
     <script type="text/javascript" src="{{ asset('scripts/main.js') }}"></script>
     <script src="https://unpkg.com/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://unpkg.com/bootstrap@5.1.0/dist/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,9 @@ Route::group(['middleware' => ['Authcheck']], function () {
     Route::get('/edit_post_view/{id}', [homeController::class, 'editPostView'])->name('postView');
 
     Route::match(array('GET', 'POST'), '/update_user/{id}', [homeController::class, 'updateUser'])->name('updateUser');
-    Route::match(array('GET', 'POST'), '/update_post/{id}', [homeController::class, 'updatePost'])->name('updatePost');
     Route::post('/delete/{id}', [homeController::class, 'delete']);
-
+    
+    Route::match(array('GET', 'POST'), '/update_post/{id}', [homeController::class, 'updatePost'])->name('updatePost');
     Route::get('/insertPost/{post}', [homeController::class, 'insertPost']);
     Route::post('editPost/{id}', [homeController::class, 'editPost']);
     Route::post('/deletePost/{id}', [homeController::class, 'deletePost']);
